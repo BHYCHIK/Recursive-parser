de842c23572c38d17eda8b46b4561bd96850c2b4
Ivan Remen <bhyk@yandex.ru>
2015-05-06 14:57:38 +0300

    Grammer done

--- lab4.py
+++ lab4.py
@@ -21,9 +21,57 @@ class RecursiveSyntaxParser(object):
 
     def is_identifier_on_top(self):
         return self.get_current_terminal().startswith('id_')
+ 
+    def op_mul(self):
+        if self.get_current_terminal() in ('*', '/', 'and', 'div', 'mod'):
+            return self.match(self.get_current_terminal())
+
+    def op_add(self):
+        if self.get_current_terminal() in ('+', '-', 'or'):
+            return self.match(self.get_current_terminal())
+
+    def sign(self):
+        if self.get_current_terminal() in ('-', '+'):
+            return self.match(self.get_current_terminal())
+
+    def rel_op(self):
+        if self.get_current_terminal() in ('==', '<>', '<', '<=', '>=', '>'):
+            return self.match(self.get_current_terminal())
+
+    def factor(self):
+        if self.is_identifier_on_top():
+            return True
+        elif self.is_on_top('('):
+            return self.match('(') and self.simple_expr() and self.match(')')
+        elif self.is_on_top('not'):
+            return self.match('not') and self.factor()
+        return False
+
+    def term_rest(self):
+        if self.op_mul():
+            return self.factor() and self.term_rest()
+        return True
+
+    def term(self):
+        return self.factor() and self.term_rest()
+
+    def single_expr_rest(self):
+        if not self.op_add():
+            return True
+        return self.term() and self.single_expr_rest()
+    
+    def simple_expr(self):
+        self.sign()
+        if not self.term():
+            return False
+        return self.single_expr_rest()
 
     def expr(self):
-        return self.match('4')
+        if not self.simple_expr():
+            return False
+        if self.rel_op():
+            return self.simple_expr()
+        return True
     
     def op(self):
         if self.is_identifier_on_top():
